4a2506d3e4865399947b17c226290404c0d7a2aa
Wessel Strengholt <wessel.strengholt@gmail.com>
2018-08-02 10:41:41 +0200

    Fix markup

--- src/Shivella/Bitly/Facade/Bitly.php
+++ src/Shivella/Bitly/Facade/Bitly.php
@@ -1,16 +1,22 @@
 <?php
-/**
+/*
  * (c) Vinícius Silva <vinicius.ls@live.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Shivella\Bitly\Facade;
-
 
 use Illuminate\Support\Facades\Facade;
 
 class Bitly extends Facade
 {
-    protected static function getFacadeAccessor() {
+    /**
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
         return 'bitly';
     }
 }
