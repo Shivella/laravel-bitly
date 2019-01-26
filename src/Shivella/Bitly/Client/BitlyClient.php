f7351f7e306347af61fb54cda96a418d080921ff
Wessel Strengholt <wessel.strengholt@gmail.com>
2019-01-26 13:54:30 +0100

    Update BitlyClient.php

--- src/Shivella/Bitly/Client/BitlyClient.php
+++ src/Shivella/Bitly/Client/BitlyClient.php
@@ -66,7 +66,7 @@ class BitlyClient
             $data = json_decode($response->getBody()->getContents(), true);
             
             if (isset($data['status_txt']) && $data['status_txt'] === 'RATE_LIMIT_EXCEEDED') {
-		throw new InvalidResponseException('You have reached the API rate limit, please try again later');
+		        throw new InvalidResponseException('You have reached the API rate limit, please try again later');
             }
 
             if (false === isset($data['data']['url'])) {
