6ccd015e2512713dbb026f28cae84a20a9c70d41
Tarun Kumar Gorai <goraitarunkumar@gmail.com>
2019-08-26 15:20:52 +0530

    fixed the isuue for new created link(for status code 201)

--- src/Shivella/Bitly/Client/BitlyClient.php
+++ src/Shivella/Bitly/Client/BitlyClient.php
@@ -65,8 +65,8 @@ class BitlyClient
                 throw new AccessDeniedException('Invalid access token');
             }
 
-            if ($response->getStatusCode() !== Response::HTTP_OK) {
-                throw new InvalidResponseException('The API does not return a 200 status code');
+            if (!($response->getStatusCode() === Response::HTTP_OK || $response->getStatusCode() === Response::HTTP_CREATED)) {
+                throw new InvalidResponseException('The API does not return a 200 or 201 status code');
             }
 
             $data = json_decode($response->getBody()->getContents(), true);
