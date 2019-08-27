bb475e1bd69da2db0dd57b8cbee8e7dc273266d1
Wessel Strengholt <wessel.strengholt@gmail.com>
2019-08-27 13:32:26 +0200

    fix syntax error statuscode check

--- src/Shivella/Bitly/Client/BitlyClient.php
+++ src/Shivella/Bitly/Client/BitlyClient.php
@@ -66,7 +66,7 @@ class BitlyClient
                 throw new AccessDeniedException('Invalid access token');
             }
             
-            if ( ! in_array($statusCode, [Response::HTTP_OK, Response::HTTP_CREATED]) {
+            if ( ! in_array($statusCode, [Response::HTTP_OK, Response::HTTP_CREATED])) {
                 throw new InvalidResponseException('The API does not return a 200 or 201 status code');
             }
 
