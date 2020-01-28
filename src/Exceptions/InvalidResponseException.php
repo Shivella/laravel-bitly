eefaa72cfc9c68191109af061ee1907acaae30d7
Paul Klimov <klimov.paul@gmail.com>
2020-01-28 19:06:06 +0200

    improve exception handling

--- src/Exceptions/InvalidResponseException.php
+++ src/Exceptions/InvalidResponseException.php
@@ -1,15 +1,17 @@
 <?php
-/*
-* (c) Wessel Strengholt <wessel.strengholt@gmail.com>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+/**
+ * (c) Wessel Strengholt <wessel.strengholt@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace Shivella\Bitly\Exceptions;
 
 /**
- * Class InvalidResponseException
+ * InvalidResponseException indicates an invalid or unexpected REST API response from the external service.
+ *
+ * @see \Shivella\Bitly\Client\BitlyClient
  */
 class InvalidResponseException extends \Exception
 {
